65682bbad05977990ab38ad3f2404d3617efa599
Dmitry Grechko <dmitry.grechko@deskree.com>
2022-09-13 21:10:59 -0400

    Added logout button functionality

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,16 @@
 import axios  from "axios";
+import store from "@/store/index";
 
 export const client = axios.create({
     baseURL: process.env.VUE_APP_API_BASE_URL,
+});
+
+client.interceptors.request.use(async (config) => {
+    const user = await store.getters["user"];
+    if (Object.prototype.hasOwnProperty.call(user, "token")) {
+        // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+        // @ts-ignore
+        config.headers["Authorization"] = `Bearer ${user.token}`;
+    }
+    return config;
 });
